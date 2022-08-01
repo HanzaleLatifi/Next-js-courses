089c188cedefd1c9690fb98467c5137962aea2c9
h8 <h.latifi03@gmail.com>
2022-08-01 21:51:51 +0430

    Link Components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from 'next/link'
 
 export default function Home() {
-  return ( <h1>this is my Home page</h1>
-  )
+  return (
+    <div>
+      <h1>this is my Home page</h1>
+      <Link href="/posts">
+        <a>go to post page</a>
+      </Link>
+    </div>
+  );
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link"
 
 function PostList() {
   return (
-    <div>postPgaes</div>
+    <div>
+      <h1>post pages</h1>
+      <Link href="/posts/1">
+        <a>post 1</a>
+      </Link>
+      <br/>
+      {/* when replace true , href is empty , back to / page when back from page */}
+      <Link href="/posts/2" replace>
+        <a>post 2</a>
+      </Link>
+    </div>
   )
 }
 
